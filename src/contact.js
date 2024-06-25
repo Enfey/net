5f5b12fa1ee6b27c96b62b549aeb4215c47ea9ce
Felix Riley-Kay <psyff1@nottingham.ac.uk>
2024-06-25 06:08:53 +0100

    contact form logic

--- src/contact.js
+++ src/contact.js
@@ -1,20 +1,72 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting by default
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-    // Perform basic validation
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let message = document.getElementById('message').value;
+const supabase = createClient('https://qviwhpnmdktfvfztfcpt.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2aXdocG5tZGt0ZnZmenRmY3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyNTIxODMsImV4cCI6MjAzNDgyODE4M30.tqkIJml1CmdVow1i5uhmSp9-UDPtFmwZJGWt26-X4ek')
+const contactForm = document.getElementById('contact-form');
+const feedbackMessageElement = document.getElementById('feedback-message');
 
-    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
-        alert('Please fill out all fields.');
+contactForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const name = contactForm.elements['name'].value.trim();
+    const email = contactForm.elements['email'].value.trim();
+    const message = contactForm.elements['message'].value.trim();
+
+    if (!validation(name, email, message)) {
         return;
     }
-
-    // You can add more specific validation for email format, etc.
-
-    // If validation passes, proceed to submit the form
-    this.submit();
+    
+    try {
+        await insertRow(name, email, message);
+        showFeedbackMessage("Message sent successfully :-)");
+        contactForm.reset();
+    } catch (error) {
+        showFeedbackMessage("An error occurred while sending the message.");
+    }
 });
+
+function validation(name, email, message) {
+    const namePattern = /^[a-zA-Z\s]{2,20}$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const messagePattern = /^.{1,250}$/;
+
+    if (name === "" || email === "" || message === "") {
+        showFeedbackMessage("All fields are required");
+        return false;
+    }
+
+    if (!namePattern.test(name)) {
+        showFeedbackMessage("Invalid name format");
+        return false;
+    }
+    if (!emailPattern.test(email)) {
+        showFeedbackMessage("Invalid email format");
+        return false;
+    }
+    if (!messagePattern.test(message)) {
+        showFeedbackMessage("Invalid message ");
+        return false;
+    }
+
+    return true;
+}
+
+async function insertRow(name, email, message) {
+    const { error } = await supabase
+        .from('contact')
+        .insert([
+            { name: name, email: email, message: message }
+        ]);
+
+    if (error) console.log('Error inserting row:', error.message);
+}
+
+function showFeedbackMessage(message) {
+    feedbackMessageElement.style.display = 'block';
+    feedbackMessageElement.textContent = message;
+    setTimeout(() => {
+        feedbackMessageElement.style.display = 'none';
+    }, 3000);
+}
+
 
   
